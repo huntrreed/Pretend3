51a6ce02096bf4b9d03eb785d3638c8e0797b0e1
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-02-12 09:38:15 -0700

    cookie

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false, 
   cookie: {
-    secure: process.env.NODE_ENV === 'production', 
+    secure: false === 'production', 
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
