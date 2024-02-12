148c694054e7560c4f82eeb52cf09ac1aead177e
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-02-11 22:53:20 -0700

    deleted stuff

--- models/User.js
+++ models/User.js
@@ -38,46 +38,11 @@ User.init(
       validate: {
         len: [8],
       },
-    },
-    hasPets: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isIn: [['Other dog(s)', 'Cat(s)', 'Other Type of Pet(s)']], 
-      },
-    },
-    fencedYard: {
-      // all, size
-      type: DataTypes.BOOLEAN,
-      // allowNull: false,
-    },
-    hasKids: {
-      /// kidFriendly
-      type: DataTypes.BOOLEAN,
-      // allowNull: false,
-    },
     fostering: {
       /// age
       type: DataTypes.BOOLEAN,
       // allowNull: false,
     },
-    previousExp: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isIn: [['No Experience', 'Some experience but never owned one', 'I have owned a dog, but it was awhile ago', 'I have extensive experience caring for dogs']], 
-      },
-    },
-    anythingElse: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    why: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-  },
-  {
     hooks: {
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
@@ -95,7 +60,9 @@ User.init(
     timestamps: true,
     modelName: 'users',
   }
+}
 );
+
 
 module.exports = User;
 
