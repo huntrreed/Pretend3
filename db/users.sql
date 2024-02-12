89a320d7546c0d126d79bbf9b17cd8faf1e39ac6
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-02-12 07:55:13 -0700

    senior

--- db/users.sql
+++ db/users.sql
@@ -4,16 +4,10 @@ USE users_db;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    userName VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    fostering BOOLEAN,
-    hasPets INT CHECK (hasPets >= 1 AND hasPets <= 4),
-    fencedYard BOOLEAN,
-    hasKids BOOLEAN,
-    previousExp INT CHECK (previousExp >= 1 AND previousExp <= 4),
-    anythingElse TEXT,
-    why TEXT,
+    allowSenior BOOLEAN,
     createdAt TIMESTAMP,
     updatedAt TIMESTAMP
 );
